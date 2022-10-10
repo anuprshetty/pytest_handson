de8886dee7c6de3ea223dc5f246b6b5326bf6eca
anuprshetty <anupshetty@gofynd.com>
2022-10-10 18:55:13 +0530

    testcases inside class

--- tests/db/test_student.py
+++ tests/db/test_student.py
@@ -2,29 +2,31 @@ import pytest
 
 from db.student import Student
 
-@pytest.fixture(scope="module")
-def db():
-    # module_setup
-    print("module_setup")
-    db = Student()
-    db.connect('student_data.json')
-    yield db
-    # module_teardown
-    print("module_teardown")
-    db.close()
+class TestStudent:
 
-@pytest.fixture
-def log():
-    # method_setup
-    print("Test case ... Started")
-    yield
-    # method_teardown
-    print("Test case... DONE")
+    @pytest.fixture(scope="module")
+    def db(self):
+        # module_setup
+        print("module_setup")
+        db = Student()
+        db.connect('student_data.json')
+        yield db
+        # module_teardown
+        print("module_teardown")
+        db.close()
 
-def test_ram_data(db, log):
-    ram_data = db.get_data('Ram')
-    assert ram_data["id"] == 1
+    @pytest.fixture
+    def log(self):
+        # method_setup
+        print("Test case ... Started")
+        yield
+        # method_teardown
+        print("Test case... DONE")
 
-def test_sham_data(db, log):
-    sham_data = db.get_data("Sham")
-    assert sham_data["id"] == 2
+    def test_ram_data(self, db, log):
+        ram_data = db.get_data('Ram')
+        assert ram_data["id"] == 1
+
+    def test_sham_data(self, db, log):
+        sham_data = db.get_data("Sham")
+        assert sham_data["id"] == 2
